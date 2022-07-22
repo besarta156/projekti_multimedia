4ba117dad7bb0d551a5a78471e79c0d83885a540
Elmedina5 <96632451+Elmedina5@users.noreply.github.com>
2022-07-22 20:56:50 +0200

    Create AOS.js

---
+++ JS/AOS.js
@@ -0,0 +1,9 @@
+AOS.init({
+  // Offset perdoret per te levizur kontrollin nga nje vend ne tjetrin
+  offset: 400, // offset (in px) from the original trigger point
+  //Delay vonesa
+  delay: 0, // values from 0 to 3000, with step 50ms
+
+  //Kohezgjatja
+  duration: 1000, // values from 0 to 3000, with step 50ms
+});
