e0fcc6031109d5e72510eadca7e7a8909186464b
Allron <52287591+lovelyroni1@users.noreply.github.com>
2022-07-25 00:01:15 +0200

    Add files via upload

---
+++ JS/Swiper.js
@@ -0,0 +1,18 @@
+//scripta per me i ndreq efektet e swiper.
+var swiper = new Swiper(".swiper-container", {
+    effect: "coverflow",
+    grabCursor: true,
+    centeredSlides: true,
+    slidesPerView: "auto",
+    coverflowEffect: {
+      rotate: 30,
+      stretch: 0,
+      depth: 500,
+      modifier: 1,
+      slideShadows: true,
+    },
+    pagination: {
+      el: ".swiper-pagination",
+    },
+  });
+  
